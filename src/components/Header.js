f85e73a86a076795038d19e2c9d5977eeac5008b
Tunahan Tatli <tunahantatl@gmail.com>
2023-04-11 14:00:03 +0300

    edit-header-v1-nav-v3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from 'react';
+//images
+import Logo from '../assets/logo.png'
 
 const Header = () => {
-  return <div className="bg-pink-300">Header</div>;
+  return <header className="py-8">
+    <div className="container mx-auto">
+      <div className='flex justify-between items-center'>
+        {/* logo */}
+        <a href="#">
+          <img src={Logo} alt="logo" />
+        </a>
+        {/* button */}
+        <button className='btn btn-sm'>Work with me</button>
+      </div>
+    </div>
+  </header>;
 };
 
 export default Header;
